57b4f507a692d5adca6d95692242ce2f8b329b1e
Francesco <francesco.bortolaso@icloud.com>
2026-07-29 23:34:49 +0200

    Update Health_bar.cs

--- Code_source_unity/New_script/Health_bar.cs
+++ Code_source_unity/New_script/Health_bar.cs
@@ -5,9 +5,9 @@ public class Health_bar : MonoBehaviour
 {
     public Slider slider; // c'est une classe Slider contient toute les informations
     
-    public void SetMaxHealth(int health) // autre methode pour la valeur maximalou donne l'information de la variable health
+    public void SetMaxHealth(int health) // autre methode pour la valeur maximal ou donne l'information de la variable health
     {
-        slider.maxValue = health; // un gameobject un slider on utilise celà pour les barre de vie et on dit que la valeu maxiam correspond à la variable health
+        slider.maxValue = health; // un gameobject un slider on utilise celà pour les barre de vie et on dit que la valeu maximaée correspond à la variable health
         slider.value = health; // c'est pour le fond cette fois ci
     }
 
