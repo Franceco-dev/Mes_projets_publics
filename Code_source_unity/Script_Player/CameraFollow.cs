bca0ae63c7a68dd1e612edfd4bd2dad6bd899427
Francesco <francesco.bortolaso@icloud.com>
2026-08-12 23:06:58 +0200

    Update CameraFollow.cs

--- Code_source_unity/Script_Player/CameraFollow.cs
+++ Code_source_unity/Script_Player/CameraFollow.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 0.125f; // vitesse de la camera
     public Vector3 offset;
 
-    void LateUpdate()
+    void LateUpdate() // methode pour que la camera suive le joueur
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = target.position + offset; // on met vectore 3 au lieux de 2 pour compter la profondeur car c'est important pour pour q'on voit bien le joueur
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
     }
