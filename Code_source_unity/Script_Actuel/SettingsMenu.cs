6bdce02ab2fd270de8c193f9dd2a158f1b21c988
Francesco <francesco.bortolaso@icloud.com>
2026-09-06 21:07:38 +0200

    Improve comments and formatting in SettingsMenu.cs

--- Code_source_unity/Script_Actuel/SettingsMenu.cs
+++ Code_source_unity/Script_Actuel/SettingsMenu.cs
@@ -3,57 +3,57 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using TMPro;
-//beaucoup de bibliothèque ici ^-^ 
-public class SettingsMenu : MonoBehaviour //ici j'ai regardé un gros tutoriel de 1h c'était l'une des parties les plus technique beacuoup de méthode, de fonctions technique bref...
+
+// Beaucoup de bibliothèques ici ^-^.
+public class SettingsMenu : MonoBehaviour // Ici j'ai regardé un gros tutoriel de 1h. C'était l'une des parties les plus techniques : beaucoup de méthodes et de fonctions techniques, bref...
 {
-    public AudioMixer audioMixer; // c'est une classe ^-^
+    public AudioMixer audioMixer; // C'est une classe ^-^.
 
     public TMP_Dropdown resolutionDropdown;
     
-    Resolution[] resolutions; // c'est un array qui pemet de mette plusieur information dedans
+    Resolution[] resolutions; // C'est un array qui permet de mettre plusieurs informations dedans.
     public void Start() 
     {
-        resolutions = Screen.resolutions; // on trouve toute les possiblités de reslution de l'écran du joueur
+        resolutions = Screen.resolutions; // On trouve toutes les possibilités de résolution de l'écran du joueur.
         if (resolutionDropdown != null)
         {
             resolutionDropdown.ClearOptions();
 
             List<string> options = new List<string>();
 
-            int currentResolutionIndex = 0; // retenir la resolution actuelle du moniteur
+            int currentResolutionIndex = 0; // Retenir la résolution actuelle du moniteur.
 
             for (int i = 0; i < resolutions.Length; i++)
             {
                 string option = resolutions[i].width + "x" + resolutions[i].height;
                 options.Add(option);
 
-                if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)// option qui correspond à la reslution du joueur
-
+                if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)// Option qui correspond à la résolution du joueur.
                 {
-                    currentResolutionIndex = i; // résolution actuelle est égale à la valeur de notre écran 
+                    currentResolutionIndex = i; // La résolution actuelle est égale à la valeur de notre écran. 
                 } 
             }
             resolutionDropdown.AddOptions(options); 
-            resolutionDropdown.value = currentResolutionIndex; // valeur active est égale à la résolution du joueur 
+            resolutionDropdown.value = currentResolutionIndex; // La valeur active est égale à la résolution du joueur. 
             resolutionDropdown.RefreshShownValue();
 
-            Screen.fullScreen = true; // etat du pleine écran à vrai
+            Screen.fullScreen = true; // État du plein écran à vrai.
         }
         
     }
-    public void SettVolume(float volume) // une methode pour le volume on donne l'information de départ à savoir la valeu du volume
+    public void SettVolume(float volume) // Une méthode pour le volume : on donne l'information de départ, à savoir la valeur du volume.
     {
         audioMixer.SetFloat("volume", volume);
     }
 
-    public void SetfullScreen(bool isFullScreen) // méthode qui permet de entrer en full screen grâce à une variable de type bool d'après ce que j'ai compris
+    public void SetfullScreen(bool isFullScreen) // Méthode qui permet d'entrer en plein écran grâce à une variable de type bool d'après ce que j'ai compris.
     {
-        Screen.fullScreen = isFullScreen; // on applique le choix du joueur vrai ou faux si il veut en pleine écran
+        Screen.fullScreen = isFullScreen; // On applique le choix du joueur (vrai ou faux) s'il veut du plein écran.
     }
 
-    public void SetResolution(int resolutionIndex) //on peut changer la resolution du jeu grâce à cette méthode
+    public void SetResolution(int resolutionIndex) // On peut changer la résolution du jeu grâce à cette méthode.
     {
         Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen); // on applique la nouvelle largeur et hauteur grâce à note choix
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen); // On applique la nouvelle largeur et hauteur grâce à notre choix.
     }
 }
