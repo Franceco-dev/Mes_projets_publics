076b312cf3503779dc58c5f2b01f47952285a5ad
Francesco <francesco.bortolaso@icloud.com>
2026-09-06 21:58:23 +0200

    Refine comments in CameraFollow script
    Corrected comments for clarity and consistency in French.

--- Code_source_unity/Script_Player/CameraFollow.cs
+++ Code_source_unity/Script_Player/CameraFollow.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.125f; // vitesse de la camera
+    public float smoothSpeed = 0.125f; // Vitesse de la caméra.
     public Vector3 offset;
 
-    void LateUpdate() // methode pour que la camera suive le joueur
+    void LateUpdate() // Méthode pour que la caméra suive le joueur.
     {
-        Vector3 desiredPosition = target.position + offset; // on met vectore 3 au lieux de 2 pour compter la profondeur car c'est important pour pour q'on voit bien le joueur
+        Vector3 desiredPosition = target.position + offset; // On met Vector3 au lieu de 2 pour compter la profondeur car c'est important pour qu'on voit bien le joueur.
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
     }
