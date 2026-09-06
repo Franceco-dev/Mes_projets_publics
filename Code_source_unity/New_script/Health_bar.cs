2921c775941be3c4b66cda42ec77ad9c20633dd4
Francesco <francesco.bortolaso@icloud.com>
2026-09-06 20:29:18 +0200

    Refactor comments for clarity and consistency

--- Code_source_unity/New_script/Health_bar.cs
+++ Code_source_unity/New_script/Health_bar.cs
@@ -1,19 +1,19 @@
-using UnityEngine; // un peu de tutoriel et un peu de documentation par ici
-using UnityEngine.UI; // blibliothèque pour le visuel
+using UnityEngine; // Un peu de tutoriel et un peu de documentation par ici.
+using UnityEngine.UI; // Bibliothèque pour le visuel.
 
 public class Health_bar : MonoBehaviour
 {
-    public Slider slider; // c'est une classe Slider contient toute les informations
+    public Slider slider; // C'est une classe Slider qui contient toutes les informations.
     
-    public void SetMaxHealth(int health) // autre methode pour la valeur maximal ou donne l'information de la variable health
+    public void SetMaxHealth(int health) // Autre méthode pour la valeur maximale où on donne l'information de la variable health.
     {
-        slider.maxValue = health; // un gameobject un slider on utilise celà pour les barre de vie et on dit que la valeu maximaée correspond à la variable health
-        slider.value = health; // c'est pour le fond cette fois ci
+        slider.maxValue = health; // Un GameObject, un slider, on utilise cela pour les barres de vie et on dit que la valeur maximale correspond à la variable health.
+        slider.value = health; // C'est pour le fond cette fois-ci.
     }
 
     public void SetHealth(int health)
     {
-        slider.value = health; // ça gère la valeur dela vie quad elle baisse
+        slider.value = health; // Ça gère la valeur de la vie quand elle baisse.
     }
 }
 
