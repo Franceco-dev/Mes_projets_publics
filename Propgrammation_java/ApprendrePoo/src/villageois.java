da5c643849be67dedbb4f09bb670244f7fe79364
Francesco <francesco.bortolaso@icloud.com>
2026-09-06 22:28:09 +0200

    Fix formatting and improve output message in villageois

--- Propgrammation_java/ApprendrePoo/src/villageois.java
+++ Propgrammation_java/ApprendrePoo/src/villageois.java
@@ -1,20 +1,16 @@
-
-
 public class villageois{
 
     public String metier;
     public String nom;
     public int pointDevie;
 
-    public villageois(String metierAttribuer ,String nomAttribuer, int pointDevieDonner){
+    public villageois(String metierAttribuer, String nomAttribuer, int pointDevieDonner){
         this.metier = metierAttribuer;
         this.nom = nomAttribuer;
         this.pointDevie = pointDevieDonner;
-
     }
 
     public void MetieretStats(){
-        System.out.println("votre villageois se nomme " + nom + " son metier est " + metier + " et il possède " + pointDevie + " points de vie");
+        System.out.println("Votre villageois se nomme " + nom + ", son métier est " + metier + " et il possède " + pointDevie + " points de vie.");
     }
-
 }
