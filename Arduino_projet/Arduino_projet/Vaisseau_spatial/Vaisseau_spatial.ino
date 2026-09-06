04518c99ee5275119dd119e566e8782d38d3e052
Francesco <francesco.bortolaso@icloud.com>
2026-09-06 22:14:24 +0200

    Update comments for clarity in Vaisseau_spatial.ino

--- Arduino_projet/Arduino_projet/Vaisseau_spatial/Vaisseau_spatial.ino
+++ Arduino_projet/Arduino_projet/Vaisseau_spatial/Vaisseau_spatial.ino
@@ -1,37 +1,37 @@
 int switchState = 0;
 
 void setup() {
-  pinMode(3, OUTPUT); // manipuler le pin 3
-  pinMode(4, OUTPUT); // manipule le pin 4
-  pinMode(5, OUTPUT); // manipule le pin 5
-  pinMode(2, INPUT); // manipule le pin 2
+  pinMode(3, OUTPUT); // Manipuler la broche 3.
+  pinMode(4, OUTPUT); // Manipule la broche 4.
+  pinMode(5, OUTPUT); // Manipule la broche 5.
+  pinMode(2, INPUT); // Manipule la broche 2.
 }
 
 void loop() {
 
-  switchState = digitalRead(2); // variable qui sert a indiquer la tension sur une broche
+  switchState = digitalRead(2); // Variable qui sert à indiquer la tension sur une broche.
   
-  // si la variable ne reçois pas de courant
+  // Si la variable ne reçoit pas de courant.
   if (switchState == LOW){ 
 
-    digitalWrite(3, HIGH); // la led verte allumer
-    digitalWrite(4, LOW); // led rouge éteind
-    digitalWrite(5, LOW); // led rouge éteind aussi
+    digitalWrite(3, HIGH); // La LED verte allumée.
+    digitalWrite(4, LOW); // LED rouge éteinte.
+    digitalWrite(5, LOW); // LED rouge éteinte aussi.
 
-  } // cette condition dit que si on appuie pas sur le bouton les leds rouges s'éteignent et la verte s'allume
+  } // Cette condition dit que si on n'appuie pas sur le bouton les LED rouges s'éteignent et la verte s'allume.
 
   else {
 
-    digitalWrite(3, LOW); // la led 3 s'éteind
-    digitalWrite(4, LOW); // euuuh deuxième led rouge éteind aussi ah enfait j'ai compris ^-^
-    digitalWrite(5, HIGH); // led rouge en gros la dernièere est allumé
+    digitalWrite(3, LOW); // La LED 3 s'éteint.
+    digitalWrite(4, LOW); // Euuuh deuxième LED rouge éteinte aussi ah en fait j'ai compris ^-^.
+    digitalWrite(5, HIGH); // LED rouge en gros la dernière est allumée.
 
-    delay(250); // 0.25 secondes par intervale
-    digitalWrite(4, HIGH); // pendant cet interval la led rouge sera allumé
+    delay(250); // 0.25 secondes par intervalle.
+    digitalWrite(4, HIGH); // Pendant cet intervalle la LED rouge sera allumée.
     digitalWrite(5, LOW);
-    delay(250); // encore attentre 0.25 secondes
+    delay(250); // Encore attendre 0.25 secondes.
 
-  } // ce sinon dis que si bouton presser la dernière led la rouge en gros s'allume
+  } // Ce sinon dit que si bouton pressé la dernière LED la rouge en gros s'allume.
 
   
 }
