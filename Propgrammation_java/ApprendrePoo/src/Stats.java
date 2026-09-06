bb2a8a0bb4386f8f5e3cb4243ece8093a76fee99
Francesco <francesco.bortolaso@icloud.com>
2026-09-06 22:26:37 +0200

    Fix typos and improve French text in Stats.java

--- Propgrammation_java/ApprendrePoo/src/Stats.java
+++ Propgrammation_java/ApprendrePoo/src/Stats.java
@@ -5,21 +5,21 @@ public class Stats {
         Scanner sc = new Scanner(System.in);
 
         while (true){
-            boolean VoireClasse = false;
+            boolean VoirClasse = false;
 
-            System.out.println("Bonjour, voulez vous voire les differentes classes et personnages disponible ??? tapez oui ou non !!!! ");
+            System.out.println("Bonjour, voulez-vous voir les différentes classes et personnages disponibles ? Tapez oui ou non ! ");
             String confirmationClasse = sc.nextLine();
 
             if (confirmationClasse.equalsIgnoreCase("oui") ){
-                VoireClasse = true;
-                System.out.println("VILLAGEOIS METIER -------> pêcheur, forgeron, cartographe");
+                VoirClasse = true;
+                System.out.println("VILLAGEOIS MÉTIER -------> pêcheur, forgeron, cartographe");
                 System.out.println("PV ---------> 20");
                 System.out.println("===============================================================");
-                System.out.println("GUERRIER METIER ---------> fantassin, cavalier, garde");
+                System.out.println("GUERRIER MÉTIER ---------> fantassin, cavalier, garde");
                 System.out.println("PV ---------> 30");
             }
-            else if (confirmationClasse.equalsIgnoreCase("Non")){
-                System.out.println("Ce serai quand même bien de checker ;)");
+            else if (confirmationClasse.equalsIgnoreCase("non")){
+                System.out.println("Ce serait quand même bien de checker ;)");
                 break;
             }
             else{
@@ -27,20 +27,19 @@ public class Stats {
                 break;
             }
 
-            System.out.println("Choisissez à présent votre classe !!!!!!!");
+            System.out.println("Choisissez à présent votre classe !");
             String choixduJoueur = sc.nextLine();
 
             if (choixduJoueur.equalsIgnoreCase("Villageois")){
-                System.out.println("Choisissez un nom à votre villageois !!!! ");
+                System.out.println("Choisissez un nom pour votre villageois ! ");
                 String NomVillageois = sc.nextLine();
-                System.out.println(NomVillageois + ", " + NomVillageois + " bien sûr maintenant choisissez votre metier ");
+                System.out.println(NomVillageois + ", " + NomVillageois + " bien sûr ! Maintenant, choisissez votre métier : ");
                 String choixClasse = sc.nextLine();
 
                 if (choixClasse.equalsIgnoreCase("pêcheur")){
                     villageois pecheur = new villageois(choixClasse, NomVillageois, 20);
                     pecheur.MetieretStats();
                 }
-
                 else if (choixClasse.equalsIgnoreCase("forgeron")){
                     villageois forgeron = new villageois(choixClasse, NomVillageois, 20);
                     forgeron.MetieretStats();
@@ -50,40 +49,35 @@ public class Stats {
                     cartographe.MetieretStats();
                 }
                 else{
-                    System.out.println("Il semble que il manque quelque chose ");
+                    System.out.println("Il semble qu'il manque quelque chose.");
                 }
             }
 
             else if(choixduJoueur.equalsIgnoreCase("Guerrier")){
-                System.out.println("Choississez un nom à votre guerrier !!!! ");
+                System.out.println("Choisissez un nom pour votre guerrier ! ");
                 String NomGuerrier = sc.nextLine();
-                System.out.println(NomGuerrier + " interressant et quelle sera le metier de " + NomGuerrier);
+                System.out.println(NomGuerrier + "... Intéressant ! Et quel sera le métier de " + NomGuerrier + " ?");
                 String choixMetier = sc.nextLine();
 
                 if (choixMetier.equalsIgnoreCase("fantassin")){
-                    guerrier fantassin = new guerrier(choixMetier, NomGuerrier  , 30);
+                    guerrier fantassin = new guerrier(choixMetier, NomGuerrier, 30);
                     fantassin.guerrierStats();
                 }
-
                 else if(choixMetier.equalsIgnoreCase("cavalier")){
                     guerrier cavalier = new guerrier(choixMetier, NomGuerrier, 30);
                     cavalier.guerrierStats();
                 }
-
                 else if(choixMetier.equalsIgnoreCase("garde")){
                     guerrier garde = new guerrier(choixMetier, NomGuerrier, 30);
-                    garde.guerrierStats();
+                    guerrier.guerrierStats();
                 }
                 else{
-                    System.out.println("Il semble qu'il manque quelque chose");
+                    System.out.println("Il semble qu'il manque quelque chose.");
                 }
-
-
             }
             else{
-                System.out.println("Veuillez svp remplir votre personnage");
+                System.out.println("Veuillez svp configurer votre personnage.");
             }
-
         }
     }
 }
