926123b9f842fc379d5d0c6bb15e8b2bc1ee67e2
Francesco <francesco.bortolaso@icloud.com>
2026-09-06 22:11:32 +0200

    Improve comments in Clignoter_led.ino
    Updated comments for clarity and consistency.

--- Arduino_projet/Arduino_projet/Clignoter_led/Clignoter_led.ino
+++ Arduino_projet/Arduino_projet/Clignoter_led/Clignoter_led.ino
@@ -1,14 +1,14 @@
-void setup(){ // départ comme dans unity
+void setup(){ // Départ comme dans Unity.
 
-  pinMode(3, OUTPUT); // c'est une fonction qui permet de manipuler un pin
+  pinMode(3, OUTPUT); // C'est une fonction qui permet de manipuler une broche.
 
 }
 
-void loop(){ // pendant le programme comme dans unity
+void loop(){ // Pendant le programme comme dans Unity.
 
-  digitalWrite(3, HIGH); // une autre fonction  qui permet d'envoyer en boucle du courant en vrai c++ arduino c'est simple
-  delay(500); // facile c'est le delay 500 miliscondes
-  digitalWrite(3, LOW); // une fonction qui enlève le courant 
-  delay(500); // on enlève le courant pendant 500 millisecondes littéralement
+  digitalWrite(3, HIGH); // Une autre fonction qui permet d'envoyer en boucle du courant, en vrai le C++ Arduino c'est simple.
+  delay(500); // Facile : c'est le délai de 500 millisecondes.
+  digitalWrite(3, LOW); // Une fonction qui enlève le courant.
+  delay(500); // On enlève le courant pendant 500 millisecondes littéralement.
 
 }
