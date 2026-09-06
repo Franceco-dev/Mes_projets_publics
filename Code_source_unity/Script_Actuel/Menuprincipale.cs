c24ed053e182b3a7a5973df4d591a162c8f1904a
Francesco <francesco.bortolaso@icloud.com>
2026-09-06 20:42:55 +0200

    Add punctuation to comments in MenuPrincipal class

--- Code_source_unity/Script_Actuel/Menuprincipale.cs
+++ Code_source_unity/Script_Actuel/Menuprincipale.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuPrincipal : MonoBehaviour // Corrigé : Principal (sans e)
+public class MenuPrincipal : MonoBehaviour 
 {
-    public string levelToLoad; // Corrigé : ToLoad (L majuscule). Variable de type chaîne de caractères
+    public string levelToLoad; // Variable de type chaîne de caractères.
 
     public GameObject settingsWindow;
     
-    public void StartGame() // Méthode pour commencer le jeu
+    public void StartGame() // Méthode pour commencer le jeu.
     {
-        SceneManager.LoadScene(levelToLoad); // On charge la scène pour commencer le jeu
+        SceneManager.LoadScene(levelToLoad); // On charge la scène pour commencer le jeu.
     }
 
-    public void SettingButton() // Méthode pour interagir avec settingsWindow
+    public void SettingButton() // Méthode pour interagir avec settingsWindow.
     {
-        settingsWindow.SetActive(true); // Si on clique sur les options, on active la fenêtre
+        settingsWindow.SetActive(true); // Si on clique sur les options, on active la fenêtre.
     }
 
     public void CloseSettingsWindow()
@@ -22,8 +22,8 @@ public class MenuPrincipal : MonoBehaviour // Corrigé : Principal (sans e)
         settingsWindow.SetActive(false);
     }
 
-    public void QuitGame() // Méthode qui permet de quitter le jeu 
+    public void QuitGame() // Méthode qui permet de quitter le jeu .
     {
-        Application.Quit(); // On quitte l'application tout simplement ^-^
+        Application.Quit(); // On quitte l'application tout simplement ^-^.
     }
 }
